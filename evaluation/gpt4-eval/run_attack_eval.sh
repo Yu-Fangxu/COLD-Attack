8ba677150bdc1e15f361f961005d26d98ca23d32
Fangxu Yu <58104156+Yu-Fangxu@users.noreply.github.com>
2024-07-07 14:12:21 +0800

    Create run_attack_eval.sh

---
+++ evaluation/gpt4-eval/run_attack_eval.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+export OPENAI_API_KEY=sk-aa
+# Replace OPENAI_API_KEY with your open-ai key
+
+# Set the folder containing your model output files
+MODEL_OUTPUT_FOLDER=$1
+
+# Set the folder where you'd like to save evaluation outputs
+EVAL_OUTPUT_FOLDER=$2
+
+# Array of filenames
+# Loop through each filename and run the commands
+for filename in "$MODEL_OUTPUT_FOLDER"/processed_*.json; do
+  echo "Processing $filename ..."
+
+  just_eval \
+      --mode "score_safety" \
+      --subtask "attack" \
+      --model "gpt-4-0314" \
+      --first_file "${filename}" \
+      --output_file "${filename}-score_multi.gpt4.json"
+
+  just_eval --report_only --mode "score_multi" \
+            --output_file "${filename}-score_multi.gpt4.json"
+
+  echo "Done with $filename."
+done
